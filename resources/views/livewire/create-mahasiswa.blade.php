c09c4ca6b01d7fef82a306e1a3e9f035bb83ee94
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2021-05-01 00:26:01 +0700

    membuat tabel

--- resources/views/livewire/create-mahasiswa.blade.php
+++ resources/views/livewire/create-mahasiswa.blade.php
@@ -13,7 +13,8 @@
                         <div class="px-4 py-5 bg-white sm:p-6">
                             <div class="grid grid-cols-6 gap-6">
                                 <div class="col-span-6 sm:col-span-3">
-                                    <label for="nama" class="block text-sm font-medium text-gray700">Nama Mahasiswa</label>
+                                    <label for="nama" class="block text-sm font-medium text-gray700">Nama
+                                        Mahasiswa</label>
                                     <input wire:model="nama" type="text" name="nama" id="nama" autoc omplete="nama"
                                         class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block wfull shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
@@ -24,12 +25,15 @@
                                 </div>
                                 <div class="col-span-6 sm:col-span-3">
                                     <label for="alamat" class="block text-sm font-medium text-gray700">Alamat</label>
-                                    <input wire:model="alamat" type="text" name="alamat" id="alamat" autocomplete="alamat" class="mt-1 focus:ring-indigo-500 focus:border-indigo500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                    <input wire:model="alamat" type="text" name="alamat" id="alamat"
+                                        autocomplete="alamat"
+                                        class="mt-1 focus:ring-indigo-500 focus:border-indigo500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
                                 </div>
                             </div>
                         </div>
                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                            <button wire:click="createMahasiswa" class="inline-flex justifycenter py-2 px-4 border border-transparent shadow-sm text-sm font-medium roundedmd text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring2 focus:ring-offset-2 focus:ring-indigo-500">
+                            <button wire:click="createMahasiswa"
+                                class="inline-flex justifycenter py-2 px-4 border border-transparent shadow-sm text-sm font-medium roundedmd text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring2 focus:ring-offset-2 focus:ring-indigo-500">
                                 Save
                             </button>
                         </div>
@@ -38,4 +42,37 @@
             </div>
         </div>
     </div>
+
+</div>
+
+<div class="rounded-t-xl overflow-hidden bg-gradient-to-r from-emerald-50 to-teal-100 p-10">
+    <table class="table-hover">
+        <thead>
+            <tr>
+                <th class="w-1/3 ... py-2 text-emerald-600">NAMA</th>
+                <th class="w-1/4 ... py-2 text-emerald-600">NIM</th>
+                <th class="w-1/2 ... py-2 text-emerald-600">ALAMAT</th>
+                <th class="w-1/2 ... py-2 text-emerald-600">ACTION</th>
+                <th class="w-1/2 ... py-2 text-emerald-600"></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-emerald-200">
+                <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">Arif Budiman Arrosyid</td>
+                <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">20180140119</td>
+                <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">Klapasawit, Purwojati, Banyumas</td>
+                <td class="btn border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
+                    <button class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Edit
+                      </button>
+                </td>
+                <td class="btn border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
+                    <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        Delete
+                      </button>
+                </td>
+            </tr>
+
+        </tbody>
+    </table>
 </div>
