6a21df380c58a8564c3432aea3ceff343dc8e68e
Arif Budiman Arrosyid <48200892+arifbudimanarrosyid@users.noreply.github.com>
2021-05-02 01:26:48 +0700

    Delete dan Edit Fix

--- app/Http/Livewire/CreateMahasiswa.php
+++ app/Http/Livewire/CreateMahasiswa.php
@@ -1,30 +1,58 @@
 <?php
 
 namespace App\Http\Livewire;
+
 use App\Models\Mahasiswa;
 use Livewire\Component;
 
 class CreateMahasiswa extends Component
 {
+    public $select_id;
     public $nama;
     public $nim;
     public $alamat;
+    public $updateMode = false;
 
     public function render()
     {
         return view('livewire.create-mahasiswa', ['dataMahasiswa' => Mahasiswa::all()]);
-        
     }
 
-    public function createMahasiswa()
+
+    public function destroy($id)
     {
-        Mahasiswa::create([
+        Mahasiswa::where('id', $id)->delete();
+        return redirect('/mahasiswa');
+    }
+    public function store()
+    {
+
+        $this->validate([
+            'nama' => 'required',
+            'nim' => 'required',
+            'alamat' => 'required'
+        ]);
+        Mahasiswa::updateOrCreate(['id' => $this->select_id], [
             'nama' => $this->nama,
             'nim' => $this->nim,
             'alamat' => $this->alamat
         ]);
+
+        $this->select_id = "";
         $this->nama = "";
         $this->nim = "";
         $this->alamat = "";
+
+        redirect('/mahasiswa');
+    }
+
+    public function edit($id)
+    {
+        $record = Mahasiswa::findOrFail($id);
+
+        $this->select_id = $id;
+        $this->nama = $record->nama;
+        $this->nim = $record->nim;
+        $this->alamat = $record->alamat;
     }
 }
--- resources/views/livewire/create-mahasiswa.blade.php
+++ resources/views/livewire/create-mahasiswa.blade.php
@@ -1,14 +1,15 @@
-<div>
-    <div class="mt-10 sm:mt-0">
-        <div class="md:grid md:grid-cols-3 md:gap-6">
-            <div class="md:col-span-1">
-                <div class="px-4 sm:px-5 py-5">
-                    <h3 class="text-lg font-medium leading-6 text-gray900">Data Mahasiswa</h3>
-                    <p class="mt-1 text-sm text-gray-600">Fakultas Teknik UMY</p>
+<form>
+    <div>
+        <div class="mt-10 sm:mt-0">
+            <div class="md:grid md:grid-cols-3 md:gap-6">
+                <div class="md:col-span-1">
+                    <div class="px-4 sm:px-5 py-5">
+                        <h3 class="text-lg font-medium leading-6 text-gray900">Data Mahasiswa</h3>
+                        <p class="mt-1 text-sm text-gray-600">Fakultas Teknik UMY</p>
+                    </div>
                 </div>
-            </div>
-            <div class="mt-5 md:mt-0 md:col-span-2">
-                <form>
+                <div class="mt-5 md:mt-0 md:col-span-2">
+
                     <div class="shadow overflow-hidden sm:rounded-md">
                         <div class="px-4 py-5 bg-white sm:p-6">
                             <div class="grid grid-cols-6 gap-6">
@@ -32,54 +33,59 @@
                             </div>
                         </div>
                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                            <button wire:click="createMahasiswa"
+                            <button wire:click="store()"
                                 class="inline-flex justifycenter py-2 px-4 border border-transparent shadow-sm text-sm font-medium roundedmd text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring2 focus:ring-offset-2 focus:ring-indigo-500">
                                 Save
                             </button>
                         </div>
                     </div>
-                </form>
+
+                </div>
             </div>
         </div>
+
     </div>
 
-</div>
+    <div class="rounded-t-xl overflow-hidden bg-gradient-to-r from-emerald-50 to-teal-100 p-10">
+        <table class="table-hover">
+            <thead>
 
-<div class="rounded-t-xl overflow-hidden bg-gradient-to-r from-emerald-50 to-teal-100 p-10">
-    <table class="table-hover">
-        <thead>
 
-            
-            <tr>
-                <th class="w-1/3 ... py-2 text-emerald-600">NAMA</th>
-                <th class="w-1/4 ... py-2 text-emerald-600">NIM</th>
-                <th class="w-1/2 ... py-2 text-emerald-600">ALAMAT</th>
-                <th class="w-1/2 ... py-2 text-emerald-600">ACTION</th>
-                <th class="w-1/2 ... py-2 text-emerald-600"></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ( $dataMahasiswa as $Mahasiswa)
-            
-            
-            <tr class="bg-emerald-200">
-                <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{{ $Mahasiswa -> nama }}</td>
-                <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{{ $Mahasiswa -> nim }}</td>
-                <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{{ $Mahasiswa -> alamat }}</td>
-                <td class="btn border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    <button class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Edit
-                    </button>
-                </td>
-                <td class="btn border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
-                    <button class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                        Delete
-                    </button>
-                </td>
-            </tr>
-            @endforeach
-            
-            {{-- @foreach($data_mahasiswa as $mahasiswa)
+                <tr>
+                    <th class="w-1/3 ... py-2 text-emerald-600">NAMA</th>
+                    <th class="w-1/4 ... py-2 text-emerald-600">NIM</th>
+                    <th class="w-1/2 ... py-2 text-emerald-600">ALAMAT</th>
+                    <th class="w-1/2 ... py-2 text-emerald-600">ACTION</th>
+                    <th class="w-1/2 ... py-2 text-emerald-600"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($dataMahasiswa as $Mahasiswa)
+
+
+                    <tr class="bg-emerald-200">
+                        <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
+                            {{ $Mahasiswa->nama }}</td>
+                        <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
+                            {{ $Mahasiswa->nim }}</td>
+                        <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
+                            {{ $Mahasiswa->alamat }}</td>
+                        <td class="btn border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
+                            <button type="button" wire:click='edit({{ $Mahasiswa->id }})'
+                                class="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Edit
+                            </button>
+                        </td>
+                        <td class="btn border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
+                            <button type="button" wire:click='destroy({{ $Mahasiswa->id }})'
+                                class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+
+                {{-- @foreach ($data_mahasiswa as $mahasiswa)
                 <tr>
                     <td>{{$mahasiswa->nama}}</td>
                     <td>{{$mahasiswa->nim}}</td>
@@ -89,6 +95,7 @@
     
                 </tr>
                 @endforeach --}}
-        </tbody>
-    </table>
-</div>
+            </tbody>
+        </table>
+    </div>
+</form>
